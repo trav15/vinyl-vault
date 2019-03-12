ef78e6de89b8d7cb0488bc096be04a77581c5c10
trav15 <travistokuyama@gmail.com>
2019-03-12 11:16:53 -0400

    Update RecordsController

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,13 +1,28 @@
 class RecordsController < ApplicationController
   def index
+    records = Record.order("created_at DESC")
+    render json: records
   end
 
   def create
+    record = Record.create(record_param)
+    render json: record
   end
 
   def update
+    record = Record.find(params[:id])
+    record.update_attributes(record_param)
+    render json: record
   end
 
   def destroy
+    record = Record.find(params[:id])
+    record.destroy
+    head :no_content, status: :ok
   end
+  
+  private
+    def record_param
+      params.require(:record).permit(:title, :artist, :cover_url)
+    end
 end
