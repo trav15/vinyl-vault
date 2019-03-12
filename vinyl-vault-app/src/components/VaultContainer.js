8079cb3badfa0d4bdedd75686dfbb11f6b49d68b
trav15 <travistokuyama@gmail.com>
2019-03-12 15:02:11 -0400

    Use fetch to retrieve data from Rails API

--- vinyl-vault-app/src/components/VaultContainer.js
+++ vinyl-vault-app/src/components/VaultContainer.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 
 class VaultContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      albums: []
+    }
+  }
+
+  getAlbums() {
+    fetch('/api/vault/albums')
+    .then(resp => resp.json())
+    .then(albums => {
+      this.setState({ albums: albums });
+    })
+  }
+
+  componentDidMount() {
+    this.getAlbums()
+  }
+
   render() {
     return (
-      <div>
-	<div className="inputContainer">
-	  <input className="taskInput" type="text"
-	    placeholder="Add a record to your vault" maxLength="50" />
-	</div>
-	<div className="listWrapper">
-	   <ul className="albumList">
-	   </ul>
-	</div>
-      </div>
+    <div>
+    	<div className="inputContainer">
+    	  <input className="taskInput" type="text"
+    	    placeholder="Add a record to your vault" maxLength="50" />
+    	</div>
+    	<div className="listWrapper">
+    	   <ul className="albumList">
+    	   </ul>
+    	</div>
+    </div>
     )
+    }
   }
-}
 
 export default VaultContainer
