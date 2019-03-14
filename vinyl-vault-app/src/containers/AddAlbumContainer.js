0646bf23a5f96b2c077b4e4a089c15aa15ca814b
trav15 <travistokuyama@gmail.com>
2019-03-14 17:39:10 -0400

    Add success message to AddAlbumContainer

--- vinyl-vault-app/src/containers/AddAlbumContainer.js
+++ vinyl-vault-app/src/containers/AddAlbumContainer.js
@@ -3,14 +3,20 @@ import axios from 'axios'
 import { connect } from 'react-redux'
 import { addAlbum } from '../actions/actionCreators'
 import AlbumForm from '../components/AlbumForm'
+import ShowAlbum from '../components/ShowAlbum'
 
 class addAlbumContainer extends Component {
+  state = {
+    message: ""
+  }
 
   addAlbum = (fields) => {
     axios.post('/api/vault/albums', {album: fields})
     .then(response => {
-      console.log(response.data)
       this.props.dispatch(addAlbum(response.data))
+    if (response.status === 200 && response.statusText === 'OK'){
+        this.setState({message: 'Successfully Added Album To Collection'})
+    }
     })
     .catch(error => console.log(error))
   }
@@ -23,6 +29,7 @@ class addAlbumContainer extends Component {
 
     return (
       <div className="album-form">
+        <div className="result">{ this.state.message }</div>
         <AlbumForm onSubmit={fields => this.onSubmit(fields)}/>
       </div>
     )
