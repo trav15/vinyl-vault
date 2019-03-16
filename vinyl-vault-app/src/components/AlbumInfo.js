04fe4c15ffcad69490b2fd2e4822da964f5a9200
trav15 <travistokuyama@gmail.com>
2019-03-16 11:28:11 -0400

    Create AlbumInfo componenet to display all album data

---
+++ vinyl-vault-app/src/components/AlbumInfo.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Col, Card, CardTitle , Button } from 'react-materialize'
+import AlbumForm from './AlbumForm'
+
+class AlbumInfo extends Component {
+  render() {
+
+    const album = this.props.currentalbum;
+    const genericCoverURL = "http://canaanmedia.com/wp-content/uploads/2014/09/12.jpg"
+    album.cover_url = album.cover_url == "" ?  genericCoverURL : album.cover_url
+    const albumURL = `/albums/${album.id}`
+
+    return (
+      <li className="album">
+        <Col>
+          <Card header={<CardTitle image={album.cover_url} waves='light'/>}
+              title = {album.title}>
+              <h3>{album.artist}</h3>
+              <p>{album.notes}</p>
+              <AlbumForm />
+              <div>
+                <Button waves='light' className="deleteAlbumBtn" onClick={(e) => this.props.deleteAlbum(album.id)}>Remove Album</Button>
+              </div>
+          </Card>
+        </Col>
+      </li>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+	return {
+		album: state.album
+	}
+}
+
+export default connect(mapStateToProps)(AlbumInfo);
