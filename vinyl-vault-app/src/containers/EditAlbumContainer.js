9e4f3a8e7ce74ef07c1302720c9848844ce2287a
trav15 <travistokuyama@gmail.com>
2019-03-19 16:09:28 -0400

    Remove unused components and stronger comparison

--- vinyl-vault-app/src/containers/EditAlbumContainer.js
+++ vinyl-vault-app/src/containers/EditAlbumContainer.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import axios from 'axios'
 import { loadAlbum, editAlbum } from '../actions/actionCreators'
-import { Col, Card, CardTitle , Button } from 'react-materialize'
 import EditAlbum from '../components/EditAlbum'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class EditAlbumContainer extends Component {
 
@@ -31,7 +29,7 @@ class EditAlbumContainer extends Component {
   render() {
     const album = this.props.album
     const genericCoverURL = "http://canaanmedia.com/wp-content/uploads/2014/09/12.jpg"
-    album.cover_url = album.cover_url == "" ?  genericCoverURL : album.cover_url
+    album.cover_url = album.cover_url === "" ?  genericCoverURL : album.cover_url
 
     return (
       <ul>
