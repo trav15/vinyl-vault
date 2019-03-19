2a7e74f556569f4c2b929afa7be37968fc35ff54
trav15 <travistokuyama@gmail.com>
2019-03-19 16:53:50 -0400

    Remove profile link from App

--- vinyl-vault-app/src/App.js
+++ vinyl-vault-app/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import VaultNavbar from './components/Navbar';
 import Home from './components/Home';
-import Profile from './components/Profile';
 import AddAlbumContainer from './containers/AddAlbumContainer'
 import VaultContainer from './containers/VaultContainer'
 
