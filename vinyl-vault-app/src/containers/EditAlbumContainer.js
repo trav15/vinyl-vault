96764d19169ac5953566d64f0b38a02fdad10d0f
trav15 <travistokuyama@gmail.com>
2019-04-03 10:23:51 -0400

    Build out Edit Album

--- vinyl-vault-app/src/containers/EditAlbumContainer.js
+++ vinyl-vault-app/src/containers/EditAlbumContainer.js
@@ -1,17 +1,68 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
-import { editAlbum } from '../actions/actionCreators'
+import { loadAlbum, editAlbum } from '../actions/actionCreators'
 import AlbumForm from '../components/AlbumForm'
+import { Row, Input, Button, Icon } from 'react-materialize'
 
 class editAlbumContainer extends Component {
+  getAlbum() {
+    axios.get(`/api/vault/${this.props.match.url}`)
+    .then(response => {
+      this.props.dispatch(loadAlbum(response.data));
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.getAlbum()
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+  }
 
   render() {
-
+    console.log(this.props.albums)
     return (
       <div className="album-form">
         <h2>HELLO</h2>
-        <AlbumForm />
+        <form>
+        <div className="panel panel-default">
+        </div>
+        <Row className="add-album-form">
+          <Input
+            s={6}
+            name="title"
+            label="Album Title"
+            class="active"
+            value={this.props.albums.title}
+            onChange={e => this.change(e)}
+          />
+          <Input
+            s={6}
+            name="artist"
+            label="Artist"
+            value={this.props.albums.artist}
+            onChange={e => this.change(e)}
+          />
+          <Input
+            s={12}
+            name="cover_url"
+            label="Cover URL (Optional)"
+            value={this.props.albums.cover_url}
+            onChange={e => this.change(e)}
+          />
+          <Input
+            s={12}
+            name="notes"
+            label="Notes (Optional)"
+            value={this.props.albums.notes}
+            onChange={e => this.change(e)}
+          />
+          <Button waves='light' onClick={e => this.onSubmit(e)}>Add Album<Icon left>library_add</Icon></Button>
+        </Row>
+        </form>
       </div>
     )
     }
