4eb145c70104f738d5c7f8995fb60b439cf7eda5
trav15 <travistokuyama@gmail.com>
2019-04-03 11:27:47 -0400

    Add Edit Album button to Card

--- vinyl-vault-app/src/components/AlbumCardReveal.js
+++ vinyl-vault-app/src/components/AlbumCardReveal.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Button } from 'react-materialize'
+import { Link } from 'react-router-dom';
 
 const AlbumCardReveal = ({album, deleteAlbum}) => (
     <div>
       <p>{album.notes}</p>
+      <Link to={`/albums/${album.id}`}><Button waves='light'>Edit Album</Button></Link>
       <Button waves='light' className="deleteAlbumBtn" onClick={(e) => deleteAlbum(album.id)}>Remove Album</Button>
     </div>
 )
